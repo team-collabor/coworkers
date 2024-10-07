e13e29e79fbd468a3a9d4a06b37d6a04a09b2a78
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-07 23:35:06 +0900

    feat: Prettier와 ESLint 통합 및 TypeScript 버전 업데이트
    - 일관된 코드 포맷팅을 위해 ESLint 설정에 Prettier 플러그인 추가.
    - ESLint 규칙에 최대 줄 길이를 80자로 설정.
    - 호환성을 위해 TypeScript 버전을 5.5.4로 업데이트.
    - RootLayout.tsx에서 줄 길이 규칙에 맞게 JSX 포맷 조정.
    - 종속성 변경 사항을 반영하기 위해 package-lock.json 업데이트.

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="tab:mx-6 mob:mx-[0.81rem] ove mx-auto flex min-h-screen max-w-[75rem] flex-col items-center">
+    <main
+      className="mx-auto flex min-h-screen max-w-[75rem] flex-col items-center
+								tab:mx-6 mob:mx-[0.81rem]"
+    >
       {children}
     </main>
   );
