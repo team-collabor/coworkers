ebe8ec14df03b365b5cd4cfd2788d647afbe0c8d
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-08 00:23:39 +0900

    feat: QueryProvider 추가 #7

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import RootLayout from '@/components/layouts/RootLayout';
+import { QueryProvider } from '@/providers/QueryProvider';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
+    <QueryProvider>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </QueryProvider>
   );
 }
