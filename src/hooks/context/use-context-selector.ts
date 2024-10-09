558f49ba22ca40966442a296e2714719182b85ed
homerabbit <92077507+me92years@users.noreply.github.com>
2024-10-09 18:37:19 +0900

    Feat : hooks/context/use-context-selector.ts 추가

---
+++ src/hooks/context/use-context-selector.ts
@@ -0,0 +1,9 @@
+import { type Context, useContext } from "react";
+
+export const useContextSelector = <T>(Context: Context<T>): T => {
+  const context = useContext<T>(Context);
+  if (!context) {
+    throw new Error("모달 컨텍스트는 프로바이더 내부에서 사용되어야 합니다.");
+  }
+  return context;
+};
