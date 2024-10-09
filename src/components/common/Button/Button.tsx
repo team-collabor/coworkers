c314ef4b5ec26923d5cca76237e82ed2ebd1f77d
seunghyun <tmdgus7048@naver.com>
2024-10-09 23:40:22 +0900

    feat: enum사용

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,15 +1,51 @@
 import clsx from 'clsx';
 import { ButtonHTMLAttributes } from 'react';
 
+enum ButtonStyle {
+  Box = 'box',
+  Floating = 'floating',
+}
+enum TextColor {
+  White = 'white',
+  Green = 'green',
+  Gray = 'gray',
+}
+enum TextSize {
+  Large = 'lg',
+  Medium = 'md',
+  Small = 'sm',
+}
+enum ButtonWidth {
+  Full = 'full',
+  Fit = 'fit',
+}
+enum ButtonBackgroundColor {
+  Green = 'green',
+  White = 'white',
+  Red = 'red',
+  None = 'none',
+}
+enum ButtonBorderColor {
+  Green = 'green',
+  Gray = 'gray',
+  None = 'none',
+}
+enum ButtonPadding {
+  Large = 'lg',
+  Medium = 'md',
+  Small = 'sm',
+  ExtraSmall = 'xs',
+}
+
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
-  buttonStyle: 'box' | 'floating';
-  textColor: 'white' | 'green' | 'gray';
-  textSize: 'lg' | 'md' | 'sm';
-  buttonWidth: 'full' | 'fit';
-  buttonBackgroundColor: 'green' | 'white' | 'red' | 'none';
-  buttonBorderColor: 'green' | 'gray' | 'none';
-  buttonPadding: 'lg' | 'md' | 'sm' | 'xs';
+  buttonStyle: ButtonStyle;
+  textColor: TextColor;
+  textSize: TextSize;
+  buttonWidth: ButtonWidth;
+  buttonBackgroundColor: ButtonBackgroundColor;
+  buttonBorderColor: ButtonBorderColor;
+  buttonPadding: ButtonPadding;
   type?: 'button' | 'submit';
   className?: string;
 };
@@ -34,39 +70,44 @@ function Button({
         'flex items-center justify-center break-keep border-[1px] border-solid',
         {
           // buttonStyle
-          'gap-2.5 rounded-xl  py-3.5 text-lg-semibold': buttonStyle === 'box',
-          'gap-1 rounded-full px-5 py-3.5': buttonStyle === 'floating',
+          'gap-2.5 rounded-xl  py-3.5 text-lg-semibold':
+            buttonStyle === ButtonStyle.Box,
+          'gap-1 rounded-full px-5 py-3.5':
+            buttonStyle === ButtonStyle.Floating,
           // buttonBackgroundColor
           [`bg-brand-primary hover:bg-interaction-hover
             active:bg-interaction-pressed disabled:bg-interaction-inactive`]:
-            buttonBackgroundColor === 'green',
-          'bg-inverse': buttonBackgroundColor === 'white',
-          'bg-status-danger': buttonBackgroundColor === 'red',
-          'bg-transparent': buttonBackgroundColor === 'none',
+            buttonBackgroundColor === ButtonBackgroundColor.Green,
+          'bg-inverse': buttonBackgroundColor === ButtonBackgroundColor.White,
+          'bg-status-danger':
+            buttonBackgroundColor === ButtonBackgroundColor.Red,
+          'bg-transparent':
+            buttonBackgroundColor === ButtonBackgroundColor.None,
           // textColor
-          'text-default': textColor === 'gray',
-          'text-inverse': textColor === 'white',
+          'text-default': textColor === TextColor.Gray,
+          'text-inverse': textColor === TextColor.White,
           [`text-brand-primary hover:text-interaction-hover
             active:text-interaction-pressed
-            disabled:text-interaction-inactive`]: textColor === 'green',
+            disabled:text-interaction-inactive`]: textColor === TextColor.Green,
           // textSize
-          'text-lg-semibold': textSize === 'lg',
-          'text-md-semibold': textSize === 'md',
-          'text-sm-semibold': textSize === 'sm',
+          'text-lg-semibold': textSize === TextSize.Large,
+          'text-md-semibold': textSize === TextSize.Medium,
+          'text-sm-semibold': textSize === TextSize.Small,
           // buttonWidth
-          'w-full': buttonWidth === 'full',
-          'w-fit': buttonWidth === 'fit',
+          'w-full': buttonWidth === ButtonWidth.Full,
+          'w-fit': buttonWidth === ButtonWidth.Fit,
           // buttonBorderColor
           [`border-brand-primary hover:border-interaction-hover
             active:border-interaction-pressed
-          disabled:border-interaction-inactive`]: buttonBorderColor === 'green',
-          'border-primary': buttonBorderColor === 'gray',
-          'border-none': buttonBorderColor === 'none',
+          disabled:border-interaction-inactive`]:
+            buttonBorderColor === ButtonBorderColor.Green,
+          'border-primary': buttonBorderColor === ButtonBorderColor.Gray,
+          'border-none': buttonBorderColor === ButtonBorderColor.None,
           // buttonPadding
-          'px-5 py-3.5': buttonPadding === 'lg',
-          'px-4 py-2.5': buttonPadding === 'md',
-          'px-3 py-[0.375rem]': buttonPadding === 'sm',
-          'px-1.5 py-2': buttonPadding === 'xs',
+          'px-5 py-3.5': buttonPadding === ButtonPadding.Large,
+          'px-4 py-2.5': buttonPadding === ButtonPadding.Medium,
+          'px-3 py-[0.375rem]': buttonPadding === ButtonPadding.Small,
+          'px-1.5 py-2': buttonPadding === ButtonPadding.ExtraSmall,
         },
         className
       )}
