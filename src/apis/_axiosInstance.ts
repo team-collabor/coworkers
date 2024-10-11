dab090e100db453be0f95c00a621bf83c4d1793a
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-11 21:50:33 +0900

    feat: axios 인스턴스 요청 및 응답 인터셉터 추가 #15
    - 요청 인터셉터를 추가하여 localStorage에서 accessToken을 가져와 Authorization 헤더에 포함.
    - 401 오류 발생 시 refreshToken을 사용하여 토큰 갱신을 시도하는 응답 인터셉터 구현.
    - 토큰 갱신 성공 시 새로운 accessToken으로 원래 요청을 재시도하도록 axios 인스턴스 업데이트.
    - 토큰 갱신 실패 시 localStorage를 비우고 에러를 처리하도록 설정.

--- src/apis/_axiosInstance.ts
+++ src/apis/_axiosInstance.ts
@@ -1,4 +1,8 @@
-import axios from 'axios';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+import { AuthState } from '@/store/useAuthStore';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const TEAM_ID = '8-4';
 const BASE_URL = `https://fe-project-cowokers.vercel.app/${TEAM_ID}`;
@@ -7,3 +11,71 @@ const BASE_URL = `https://fe-project-cowokers.vercel.app/${TEAM_ID}`;
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    if (typeof window !== 'undefined') {
+      const authStorage = localStorage.getItem('authStore') || '{}';
+      try {
+        const { accessToken } = JSON.parse(authStorage) as AuthState;
+        if (accessToken) {
+          const modifiedConfig = { ...config };
+          // eslint-disable-next-line @typescript-eslint/dot-notation
+          modifiedConfig.headers['Authorization'] = `Bearer ${accessToken}`;
+          return modifiedConfig;
+        }
+      } catch {
+        return config;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    // 에러 핸들링
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (responseError) => {
+    if (typeof window !== 'undefined') {
+      const originalRequest = responseError.config;
+      let refreshToken = null;
+      if (responseError.response?.status === 401 && !originalRequest._retry) {
+        const authStorage = localStorage.getItem('authStore') || '{}';
+        try {
+          const { state } = JSON.parse(authStorage);
+          refreshToken = state.refreshToken;
+        } catch {
+          return Promise.reject(responseError);
+        }
+        if (refreshToken) {
+          try {
+            // refreshToken으로 새로운 토큰 요청
+            const response = await axiosInstance.post('/auth/refresh-token', {
+              refreshToken,
+            });
+            const { accessToken } = response.data;
+
+            // Zustand store에 새로운 토큰 저장
+            localStorage.setItem(
+              'authStore',
+              JSON.stringify({ state: response.data, version: 0 })
+            );
+
+            // 새로운 accessToken으로 요청 재시도
+            originalRequest._retry = true;
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+            return await axiosInstance(originalRequest); // await 추가
+          } catch (refreshError) {
+            // 토큰 갱신 실패 시 로그아웃 처리
+            localStorage.clear();
+            return Promise.reject(refreshError);
+          }
+        }
+      }
+      return Promise.reject(responseError);
+    }
+  }
+);
