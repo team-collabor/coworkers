2c30f0ecf379b802b6edbbebe3a5667fd96da1c1
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-11 14:22:28 +0900

    feat: axios instance 추가 #48

---
+++ src/apis/_axiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const TEAM_ID = '8-4';
+const BASE_URL = `https://fe-project-cowokers.vercel.app/${TEAM_ID}`;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
