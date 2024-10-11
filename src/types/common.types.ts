2c1e3d96fcd8f6fc7f2ed3d6c2a4679f374f4660
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-11 17:50:44 +0900

    fix: common types 추가 #15

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -4,6 +4,16 @@ export type CommonTypes = {
   children: ReactNode;
   label: string;
   errorMessage: string;
+  updatedAt: Date;
+  createdAt: Date;
+  title: string;
+  id: number;
+  name: string;
+  nickname: string;
+  email: string;
+  image: string | null;
+  accessToken: string | null;
+  refreshToken: string | null;
   onClick: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   e: React.MouseEvent<HTMLElement, MouseEvent>;
 };
