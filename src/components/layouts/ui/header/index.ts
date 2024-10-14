fca837cf5e77d283009f5294afa5d93222744da9
homerabbit <92077507+me92years@users.noreply.github.com>
2024-10-14 15:59:07 +0900

    Fix : 파일명 대소문자 수정

--- src/components/layouts/ui/header/index.ts
+++ src/components/layouts/ui/header/index.ts
@@ -1,5 +1,8 @@
 import dynamic from 'next/dynamic';
 
+/**
+ * 헤더 컴포넌트
+ */
 export const Header = Object.assign(
   dynamic(async () => (await import('./ui/header.main')).default, {
     ssr: false,
