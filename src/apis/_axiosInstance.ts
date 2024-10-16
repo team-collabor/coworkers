d183e84412df3f1f2feeb3c08c07ef22c715b51e
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-16 17:12:04 +0900

    refactor: axios 인스턴스에서 로컬 스토리지 접근 제거 및 zustand 사용 #15
    - axios 요청 인터셉터에서 로컬 스토리지 접근 제거하고 zustand 의 useAuthStore를 사용하여 accessToken을 가져오도록 변경
    
    - 코드 간소화

--- src/apis/_axiosInstance.ts
+++ src/apis/_axiosInstance.ts
@@ -1,37 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-
+import { useAuthStore } from '@/store/useAuthStore';
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const TEAM_ID = '8-4';
 const BASE_URL = `https://fe-project-cowokers.vercel.app/${TEAM_ID}`;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    if (typeof window !== 'undefined') {
-      const authStorage = localStorage.getItem('authStore') || '{}';
-      try {
-        const { state } = JSON.parse(authStorage);
-        const { accessToken } = state;
-        if (accessToken) {
-          const modifiedConfig = { ...config };
-          // eslint-disable-next-line @typescript-eslint/dot-notation
-          modifiedConfig.headers['Authorization'] = `Bearer ${accessToken}`;
-          return modifiedConfig;
-        }
-      } catch {
-        return config;
-      }
+    const { accessToken } = useAuthStore.getState();
+    if (accessToken) {
+      const modifiedConfig = { ...config };
+      modifiedConfig.headers.Authorization = `Bearer ${accessToken}`;
+      return modifiedConfig;
     }
     return config;
   },
   (error) => {
-    // 에러 핸들링
     return Promise.reject(error);
   }
 );
