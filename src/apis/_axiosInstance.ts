60266326d06424b0a0cde1e442ecac5f397a9784
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-17 13:31:42 +0900

    fix: 환경 변수 추가 #73
    - 환경 변수 파일 .env.local 을 사용해야 함

--- src/apis/_axiosInstance.ts
+++ src/apis/_axiosInstance.ts
@@ -3,8 +3,8 @@
 
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-const TEAM_ID = '8-4';
-const BASE_URL = `https://fe-project-cowokers.vercel.app/${TEAM_ID}`;
+const TEAM_ID = process.env.NEXT_PUBLIC_TEAM_ID;
+const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/${TEAM_ID}`;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const axiosInstance = axios.create({
