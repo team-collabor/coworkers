aba635a0093a7b40b3397c8dc9abc9236efcb440
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-18 10:13:42 +0900

    feat: 목록 상세 페이지 추가 #18

---
+++ src/pages/[id]/tasks/index.tsx
@@ -0,0 +1,10 @@
+export default function TaskPage() {
+  return (
+    <>
+      <div className="mt-10 flex w-full tab:mt-6">
+        <h1 className="text-xl-bold text-primary">할 일</h1>
+      </div>
+      <div>날짜</div>
+    </>
+  );
+}
