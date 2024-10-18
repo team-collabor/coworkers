302b03c04df53af898f34941fb9d2997364f9e28
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-19 06:04:12 +0900

    fix: 공통컴포넌트 Canlendar 스타일 요구사항 적용 #18

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -18,25 +18,24 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
+        months:
+          'flex flex-col mob:flex-row space-y-4 mob:space-x-4 mob:space-y-0',
         month: 'space-y-4',
         caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
+        caption_label: 'text-md-regular text-inverse',
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
           buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+          'h-7 w-9 bg-transparent p-0 opacity-90 hover:bg-interaction-hover'
         ),
         nav_button_previous: 'absolute left-1',
         nav_button_next: 'absolute right-1',
         table: 'w-full border-collapse space-y-1',
         head_row: 'flex',
-        head_cell: cn(
-          'text-neutral-400 rounded-md w-9 font-normal text-[0.8rem]'
-        ),
+        head_cell: cn('text-inverse rounded-md w-[2.714rem] text-lg-semibold'),
         row: cn('flex w-full mt-2'),
         cell: cn(
-          'h-9 w-9 text-center text-sm p-0 relative',
+          'h-8 w-[2.714rem] text-center text-sm p-0 relative',
           '[&:has([aria-selected].day-range-end)]:rounded-r-md',
           '[&:has([aria-selected].day-outside)]:bg-neutral-800/50',
           '[&:has([aria-selected])]:bg-neutral-800',
@@ -46,20 +45,22 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
+          'h-9 w-10 p-0 text-md-medium text-inverse',
+          'hover:bg-interaction-hover',
+          'aria-selected:bg-brand-primary aria-selected:text-inverse'
         ),
         day_range_end: cn('day-range-end'),
         day_selected: cn(
-          'bg-neutral-50 text-neutral-900 hover:bg-neutral-50',
-          'hover:text-neutral-900 focus:bg-neutral-50 focus:text-neutral-900'
+          'bg-brand-primary text-inverse hover:bg-brand-primary',
+          'hover:text-inverse focus:bg-brand-primary focus:text-inverse'
         ),
-        day_today: cn('bg-neutral-800 text-neutral-50'),
+        day_today: cn('bg-tertiary text-inverse'),
         day_outside: cn(
-          'day-outside text-neutral-400 opacity-50',
-          'aria-selected:bg-neutral-800/50 aria-selected:text-neutral-400',
-          'aria-selected:opacity-30'
+          'day-outside text-inverse opacity-60',
+          'aria-selected:bg-brand-primary aria-selected:text-inverse',
+          'aria-selected:opacity-60'
         ),
-        day_disabled: cn('text-neutral-400 opacity-50'),
+        day_disabled: cn('text-default opacity-50'),
         day_range_middle: cn(
           'aria-selected:bg-neutral-800 aria-selected:text-neutral-50'
         ),
