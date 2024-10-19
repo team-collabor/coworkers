cb77c34ff7cee74c7e99526e331107cb42273dd8
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-19 23:57:19 +0900

    feat: 목록 상세 페이지 날짜 선택 헤더 추가 #18

--- src/pages/[id]/tasks/index.tsx
+++ src/pages/[id]/tasks/index.tsx
@@ -1,10 +1,34 @@
+import Button, {
+  ButtonBackgroundColor,
+  ButtonBorderColor,
+  ButtonPadding,
+  ButtonStyle,
+  ButtonWidth,
+  TextColor,
+  TextSize,
+} from '@/components/common/Button/Button';
+import { DatePicker } from '@/components/common/DatePicker';
+
 export default function TaskPage() {
   return (
     <>
       <section className="mt-10 flex w-full tab:mt-6">
         <h1 className="text-xl-bold text-primary">할 일</h1>
       </section>
-      <div>날짜</div>
+      <section className="mt-7 flex w-full items-center justify-between">
+        <DatePicker mode="selector" />
+        <Button
+          buttonStyle={ButtonStyle.Box}
+          buttonBackgroundColor={ButtonBackgroundColor.None}
+          textColor={TextColor.Green}
+          textSize={TextSize.Medium}
+          buttonWidth={ButtonWidth.Fit}
+          buttonPadding={ButtonPadding.ExtraSmall}
+          buttonBorderColor={ButtonBorderColor.None}
+        >
+          + 새로운 목록 추가하기
+        </Button>
+      </section>
     </>
   );
 }
