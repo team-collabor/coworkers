9a0f26c99796879f501c43753907f7988d8e6142
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-21 09:19:21 +0900

    fix: task store selectedTaskList 추가 및 세션 스토리지 persist 제거 #18

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,33 +1,36 @@
+import { TaskList } from '@/types/team';
 import { create } from 'zustand';
-import { combine, createJSONStorage, persist } from 'zustand/middleware';
+import { combine } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 export type TaskState = {
   selectedDate: Date;
+  selectedTaskList: TaskList | null;
 };
 
 export type TaskActions = {
   setSelectedDate: (date: Date) => void;
+  setSelectedTaskList: (taskList: TaskList) => void;
 };
 
 const initialState: TaskState = {
   selectedDate: new Date(),
+  selectedTaskList: null,
 };
 
 export const useTaskStore = create(
-  persist(
-    immer(
-      combine(initialState, (set) => ({
-        setSelectedDate: (date: Date) => {
-          set((state) => {
-            state.selectedDate = date;
-          });
-        },
-      }))
-    ),
-    {
-      name: 'taskStore',
-      storage: createJSONStorage(() => sessionStorage),
-    }
+  immer(
+    combine(initialState, (set) => ({
+      setSelectedDate: (date: Date) => {
+        set((state) => {
+          state.selectedDate = date;
+        });
+      },
+      setSelectedTaskList: (taskList: TaskList) => {
+        set((state) => {
+          state.selectedTaskList = taskList;
+        });
+      },
+    }))
   )
 );
