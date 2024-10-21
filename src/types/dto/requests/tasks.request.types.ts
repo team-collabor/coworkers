67775357f22e72b4de5de8a8952db5bbb3827159
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-21 09:13:27 +0900

    feat: tasks updateTaskStatus DTO 추가 #18

--- src/types/dto/requests/tasks.request.types.ts
+++ src/types/dto/requests/tasks.request.types.ts
@@ -3,3 +3,11 @@ export type GetTaskRequest = {
   taskListId: number;
   date?: string; // 날짜는 선택적이며, ISO 8601 형식의 문자열
 };
+
+export type UpdateTaskStatusRequest = {
+  groupId: number;
+  taskListId: number;
+  taskId: number;
+  done: boolean;
+  date?: string;
+};
