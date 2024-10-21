5e869195668d4fa10c679b369d21747de00309b4
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-21 09:17:45 +0900

    feat: tasks useUpdateTaskStatus 쿼리 추가 #18

--- src/queries/tasks.queries.ts
+++ src/queries/tasks.queries.ts
@@ -1,14 +1,33 @@
-import { getTask } from '@/apis/tasks.api';
-import { GetTaskRequest } from '@/types/dto/requests/tasks.request.types';
-import { useQuery } from '@tanstack/react-query';
+import { getTask, updateTaskStatus } from '@/apis/tasks.api';
+import {
+  GetTaskRequest,
+  UpdateTaskStatusRequest,
+} from '@/types/dto/requests/tasks.request.types';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { tasksQueryKeys } from './keys/tasks.keys';
 
 export const useTasks = (params: GetTaskRequest) => {
-  const { ...returns } = useQuery({
+  return useQuery({
     queryKey: tasksQueryKeys.tasks(params),
     queryFn: () => getTask(params),
     enabled: !!params.date && !!params.groupId && !!params.taskListId,
   });
+};
 
-  return { ...returns };
+export const useUpdateTaskStatus = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (params: UpdateTaskStatusRequest) =>
+      updateTaskStatus(params),
+    onSuccess: (_, params) => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({
+        queryKey: tasksQueryKeys.tasks({
+          groupId: params.groupId,
+          taskListId: params.taskListId,
+          date: params.date,
+        } as GetTaskRequest),
+      });
+    },
+  });
 };
