16dec3a5afad55e1c9c0e7f051811ee5a52e1674
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-10-24 02:32:46 +0900

    feat: addTask 관련 axios api 및 query 추가 #19

--- src/apis/tasks.api.ts
+++ src/apis/tasks.api.ts
@@ -1,11 +1,14 @@
 import {
+  AddTaskRequest,
   GetTaskRequest,
   UpdateTaskStatusRequest,
 } from '@/types/dto/requests/tasks.request.types';
 import {
+  AddTaskResponse,
   GetTaskResponse,
   UpdateTaskStatusResponse,
 } from '@/types/dto/responses/tasks.response.types';
+import _ from 'lodash';
 import { axiosInstance } from './_axiosInstance';
 
 export const getTask = async (params: GetTaskRequest) => {
@@ -15,6 +18,18 @@ export const getTask = async (params: GetTaskRequest) => {
     params: {
       date: params.date,
     },
+  });
+  return response.data;
+};
+
+export const addTask = async (params: AddTaskRequest) => {
+  // eslint-disable-next-line max-len
+  const path = `/groups/${params.groupId}/task-lists/${params.taskListId}/recurring`;
+  const requestBody = _.omit(params, ['groupId', 'taskListId']);
+  const response = await axiosInstance<AddTaskResponse>({
+    method: 'POST',
+    url: path,
+    data: requestBody,
   });
   return response.data;
 };
--- src/queries/tasks.queries.ts
+++ src/queries/tasks.queries.ts
@@ -1,8 +1,11 @@
-import { getTask, updateTaskStatus } from '@/apis/tasks.api';
+import { addTask, getTask, updateTaskStatus } from '@/apis/tasks.api';
+import { useToast } from '@/hooks/useToast';
 import {
+  AddTaskRequest,
   GetTaskRequest,
   UpdateTaskStatusRequest,
 } from '@/types/dto/requests/tasks.request.types';
+import { FrequencyType } from '@/types/tasks.types';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { tasksQueryKeys } from './keys/tasks.keys';
 
@@ -11,6 +14,46 @@ export const useTasks = (params: GetTaskRequest) => {
     queryKey: tasksQueryKeys.tasks(params),
     queryFn: () => getTask(params),
     enabled: !!params.date && !!params.groupId && !!params.taskListId,
+  });
+};
+
+export const useAddTask = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  return useMutation({
+    onMutate: (params) => {
+      if (params.frequencyType === FrequencyType.Weekly) {
+        const { monthDay, ...rest } = params;
+        return rest;
+      }
+      if (params.frequencyType === FrequencyType.Monthly) {
+        const { weekDays, ...rest } = params;
+        return rest;
+      }
+      const { weekDays, monthDay, ...rest } = params;
+      return rest;
+    },
+    mutationFn: async (params: AddTaskRequest) => addTask(params),
+    onSuccess: (_, params) => {
+      toast({
+        title: '할 일을 추가했습니다.',
+      });
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({
+        queryKey: tasksQueryKeys.tasks({
+          groupId: params.groupId,
+          taskListId: params.taskListId,
+        }),
+      });
+    },
+    onError: (error) => {
+      toast({
+        title: '할 일 추가를 실패했습니다.',
+        description: error.message,
+        variant: 'destructive',
+      });
+    },
   });
 };
 
