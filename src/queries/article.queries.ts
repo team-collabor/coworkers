1e582b303da8770883acb2bdf616c3cb4e60812b
seunghyun <tmdgus7048@naver.com>
2024-10-24 12:46:10 +0900

    feat: article useQuery,useMutation 추가

--- src/queries/article.queries.ts
+++ src/queries/article.queries.ts
@@ -1,11 +1,30 @@
-import { getArticles, postArticle } from '@/apis/article.api';
+import {
+  deleteArticleComment,
+  getArticleComments,
+  getArticleDetail,
+  getArticles,
+  likeArticle,
+  postArticle,
+  postArticleComment,
+  unlikeArticle,
+  updateArticleComment,
+} from '@/apis/article.api';
 import {
   GetArticlesParams,
+  PostArticleCommentParams,
   PostArticleParams,
 } from '@/types/dto/requests/article.request.types';
 // eslint-disable-next-line max-len
-import { ArticleListResponse } from '@/types/dto/responses/article.response.types';
-import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
+import {
+  ArticleCommentListResponse,
+  ArticleListResponse,
+} from '@/types/dto/responses/article.response.types';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
 
 export const useBestArticlesQuery = (params: GetArticlesParams) => {
   return useQuery({
@@ -37,5 +56,88 @@ export const useAllArticlesQuery = (
 export const usePostArticleMutation = () => {
   return useMutation({
     mutationFn: (article: PostArticleParams) => postArticle(article),
+  });
+};
+
+export const useGetArticleDetailQuery = (articleId: number) => {
+  return useQuery({
+    queryKey: ['articleDetail', articleId],
+    queryFn: () => getArticleDetail(articleId),
+    enabled: !Number.isNaN(articleId),
+  });
+};
+
+export const useLikeArticleMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (articleId: number) => likeArticle(articleId),
+    onSuccess: () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({ queryKey: ['articleDetail'] });
+    },
+  });
+};
+
+export const useUnlikeArticleMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (articleId: number) => unlikeArticle(articleId),
+    onSuccess: () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({ queryKey: ['articleDetail'] });
+    },
+  });
+};
+
+export const usePostArticleCommentMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: PostArticleCommentParams) => postArticleComment(data),
+    onSuccess: () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({ queryKey: ['articleComments'] });
+    },
+  });
+};
+
+export const useGetArticleCommentsQuery = (
+  articleId: number,
+  limit: number
+) => {
+  return useInfiniteQuery<ArticleCommentListResponse>({
+    queryKey: ['articleComments', articleId, limit],
+    queryFn: ({ pageParam = 0 }) =>
+      getArticleComments({ limit, cursor: pageParam as number, articleId }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextCursor || undefined,
+    enabled: !!articleId,
+  });
+};
+
+export const useDeleteArticleCommentMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (commentId: number) => deleteArticleComment(commentId),
+    onSuccess: () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({ queryKey: ['articleComments'] });
+    },
+  });
+};
+
+export const useUpdateArticleCommentMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      commentId,
+      content,
+    }: {
+      commentId: number;
+      content: string;
+    }) => updateArticleComment(commentId, content),
+    onSuccess: () => {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      queryClient.invalidateQueries({ queryKey: ['articleComments'] });
+    },
   });
 };
