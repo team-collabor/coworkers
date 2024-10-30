b809307adbebfbc80bcb79f6326cf67f5ebe4177
homerabbit <92077507+me92years@users.noreply.github.com>
2024-10-30 15:52:20 +0900

    Fix : 로그인 버튼 추가, 마이 히스토리 날짜 정렬 기능 추가.

--- src/components/layouts/ui/header/ui/header.main.tsx
+++ src/components/layouts/ui/header/ui/header.main.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable max-len */
+import { useAuthStore } from '@/store/useAuthStore';
+import Link from 'next/link';
 import HeaderList from './header.list';
 import HeaderLogo from './header.logo';
 import HeaderNav from './header.nav';
 import HeaderProfile from './header.profile';
 
 export default function HeaderMain() {
+  const { user } = useAuthStore();
   return (
     <header className="fixed inset-x-0 top-0 z-50">
       <div className="flex h-16 w-screen select-none justify-center bg-secondary text-primary">
@@ -13,8 +16,16 @@ export default function HeaderMain() {
             <HeaderList className="mr-4" />
             <HeaderLogo className="mr-4" />
           </div>
-          <HeaderNav />
-          <HeaderProfile />
+          {user ? (
+            <>
+              <HeaderNav />
+              <HeaderProfile />
+            </>
+          ) : (
+            <Link href="/signin" className="ml-auto hover:opacity-90">
+              로그인
+            </Link>
+          )}
         </div>
       </div>
     </header>
--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -2,29 +2,38 @@ import { UnoptimizedImage } from '@/components/next';
 import { useGetHistory } from '@/queries/users.queries';
 import { TTaskDone } from '@/types/temp.types';
 import { cn } from '@/utils/tailwind/cn';
+import { NoneHistory } from './NoneHistory';
+
+const parseDate = (date: string) => {
+  const [year, month, day] = date.match(/\d+/g)?.map(Number) as number[];
+  return new Date(year, month - 1, day);
+};
 
 export default function History() {
   const { data } = useGetHistory();
   if (!data || data.tasksDone.length === 0) {
-    return null;
+    return <NoneHistory />;
   }
   const { tasksDone } = data;
   const tasksByDate = tasksDone.reduce((acc, task) => {
     const date = new Date(task.date);
     const key = [
-      `${date.getFullYear()}년`,
-      `${date.getMonth() + 1}월`,
+      `${date.getFullYear()}년 `,
+      `${date.getMonth() + 1}월 `,
       `${date.getDate()}일`,
-    ].join(' ');
+    ].join('');
     acc.set(key, [...(acc.get(key) || []), task]);
     return acc;
   }, new Map<string, TTaskDone[]>());
+  const sortedTasksByDate = Array.from(tasksByDate.entries()).sort((t1, t2) => {
+    return parseDate(t1[0]).getTime() - parseDate(t2[0]).getTime();
+  });
   return (
     <>
       <h1 className="mb-6 mt-10 self-start text-xl font-bold">마이 히스토리</h1>
       <ul className="mb-16 flex w-full flex-col gap-y-10 self-start">
-        {Array.from(tasksByDate.entries()).map(
-          ([key, taskList]: [string, TTaskDone[]]) => (
+        {sortedTasksByDate.map(([key, taskList]: [string, TTaskDone[]]) => {
+          return (
             <li key={key}>
               <h2 className="mb-4">{key}</h2>
               <ul className="flex flex-col gap-y-4">
@@ -51,8 +60,8 @@ export default function History() {
                   ))}
               </ul>
             </li>
-          )
-        )}
+          );
+        })}
       </ul>
     </>
   );
