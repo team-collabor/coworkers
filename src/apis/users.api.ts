d37925771c34f0d400eca01e38c6db5605d126c4
OK <ohl2619@naver.com>
2024-11-01 17:15:23 +0900

    feat: 계정설정에 필요한api 추가

--- src/apis/users.api.ts
+++ src/apis/users.api.ts
@@ -1,6 +1,10 @@
 import {
   ResetPasswordRequest,
   SendResetPasswordEmailRequest,
+  UpdateUserImageRequest,
+  UpdateUserImageUploadRequest,
+  UpdateUserNicknameRequest,
+  UpdateUserPasswordRequest,
 } from '@/types/dto/requests/users.request.types';
 import {
   GetHistoryResponse,
@@ -8,6 +12,10 @@ import {
   GetUserResponse,
   ResetPasswordResponse,
   SendResetPasswordEmailResponse,
+  UpdateUserImageUploadResponse,
+  UpdateUserNicknameResponse,
+  UpdateUserPasswordResponse,
+  UserImageUpdateResponse,
 } from '@/types/dto/responses/users.response.types';
 import { axiosInstance } from './_axiosInstance';
 
@@ -51,6 +59,62 @@ export const getHistory = async () => {
   const response = await axiosInstance<GetHistoryResponse>({
     method: 'GET',
     url: '/user/history',
+  });
+  return response.data;
+};
+
+export const deleteUser = async (): Promise<void> => {
+  await axiosInstance<void>({
+    method: 'DELETE',
+    url: '/user',
+  });
+};
+
+export const uploadUserImage = async (
+  params: UpdateUserImageUploadRequest
+): Promise<UpdateUserImageUploadResponse> => {
+  const formData = new FormData();
+  formData.append('image', params.image);
+
+  const imageResponse = await axiosInstance<UpdateUserImageUploadResponse>({
+    method: 'POST',
+    url: '/images/upload',
+    data: formData,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return imageResponse.data; // 이미지 URL 반환
+};
+
+// 사용자 이미지 URL을 업데이트하는 함수
+export const updateUserImageUrl = async (
+  params: UpdateUserImageRequest
+): Promise<UserImageUpdateResponse> => {
+  const response = await axiosInstance<UserImageUpdateResponse>({
+    method: 'PATCH',
+    url: '/user',
+    data: params,
+  });
+
+  return response.data; // 업데이트 결과 반환
+};
+
+export const updateUserNickname = async (params: UpdateUserNicknameRequest) => {
+  const response = await axiosInstance<UpdateUserNicknameResponse>({
+    method: 'PATCH',
+    url: '/user',
+    data: params,
+  });
+  return response.data;
+};
+
+export const updateUserPassword = async (params: UpdateUserPasswordRequest) => {
+  const response = await axiosInstance<UpdateUserPasswordResponse>({
+    method: 'PATCH',
+    url: '/user/password',
+    data: params,
   });
   return response.data;
 };
