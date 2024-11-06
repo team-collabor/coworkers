b68f16a3e5c284e9cb96efa27b391c017367fe80
homerabbit <92077507+me92years@users.noreply.github.com>
2024-11-06 17:23:06 +0900

    Fix : 사이드바 이미지 추가

--- src/components/layouts/ui/membership/MembershipItem.tsx
+++ src/components/layouts/ui/membership/MembershipItem.tsx
@@ -2,6 +2,8 @@ import { ASIDE_MENU } from '@/components/layouts/consts/_aside.menu';
 import { UnoptimizedImage } from '@/components/next';
 import { Membership } from '@/types/users.types';
 import { cn } from '@/utils/tailwind/cn';
+import { ImageIcon } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { memo } from 'react';
 import { Menu } from '../menu';
@@ -20,7 +22,14 @@ export const MembershipItem = memo(
         key={groupId}
         className={cn(['relative grid grid-cols-[auto_max-content] gap-x-4'])}
       >
-        <Menu.Trigger menuId={ASIDE_MENU}>
+        <Menu.Trigger menuId={ASIDE_MENU} className="flex items-center gap-x-2">
+          {group.image ? (
+            <div className="relative size-8">
+              <Image src={group.image} alt="" fill />
+            </div>
+          ) : (
+            <ImageIcon width={32} height={32} />
+          )}
           <Link
             href={`/${groupId}`}
             type="button"
