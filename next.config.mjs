06ba4946e14bd1d5eab139d90ee6e68fc3f0f4da
wonsik <70436955+wonsik3686@users.noreply.github.com>
2024-12-03 06:43:02 +0900

    chore: svgr 관련 설정 추가 #158
    - next.config.mjs, tsconfig, svgr.d.ts 를 추가 혹은 수정하여 svgr 관련 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,6 +15,31 @@ const nextConfig = {
         hostname: '*',
       },
     ],
+  },
+  webpack: (config) => {
+    // 기존 SVG 파일 로더 규칙을 찾습니다.
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      // 기존 규칙을 재적용하되, ?url 로 끝나는 SVG 파일에만 적용합니다.
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // 나머지 *.svg 파일을 React 컴포넌트로 변환합니다.
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/],
+        }, // *.svg?url 제외
+        use: ['@svgr/webpack'],
+      }
+    );
+    return config;
   },
 };
 
