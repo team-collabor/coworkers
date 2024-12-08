bf871f0a2ff7753f0ce7bab797b208b59409380e
Injaeeee <97098671+Injaeeee@users.noreply.github.com>
2024-12-09 02:12:35 +0900

    feat : 에러바운더리 파일 설치

--- src/components/common/error/ErrorBoundary.tsx
+++ src/components/common/error/ErrorBoundary.tsx
@@ -1,1 +1,29 @@
-export default function ErrorBoundary() {}
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { Loader } from 'lucide-react';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ErrorFallback';
+
+export default function GlobalBoundary({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
+          <Suspense
+            fallback={
+              <div className="flex h-[50rem] items-center justify-center">
+                <Loader className="animate-spin size-20 text-icon-brand" />
+              </div>
+            }
+          >
+            {children}
+          </Suspense>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
+  );
+}
--- src/components/common/error/ErrorFallback.tsx
+++ src/components/common/error/ErrorFallback.tsx
@@ -1,1 +1,48 @@
-export default function ErrorFallback() {}
+import Button, {
+  ButtonBackgroundColor,
+  ButtonBorderColor,
+  ButtonPadding,
+  ButtonStyle,
+  ButtonWidth,
+  TextColor,
+  TextSize,
+} from '@/components/common/Button/Button';
+
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { FallbackProps } from 'react-error-boundary';
+
+export default function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: FallbackProps) {
+  const [redirected, setRedirected] = useState(false);
+  const router = useRouter();
+  if (
+    axios.isAxiosError(error) &&
+    error.response?.status === 401 &&
+    !redirected
+  ) {
+    router.push('/signin'); // 로그인 페이지로 리다이렉트
+    setRedirected(true);
+    return; // 더 이상 처리를 하지 않음
+  }
+  return (
+    <div>
+      <h2>{error.message}</h2>
+      <Button
+        buttonStyle={ButtonStyle.Box}
+        textColor={TextColor.Gray}
+        textSize={TextSize.Large}
+        buttonWidth={ButtonWidth.Full}
+        buttonBackgroundColor={ButtonBackgroundColor.Green}
+        buttonBorderColor={ButtonBorderColor.Green}
+        buttonPadding={ButtonPadding.Medium}
+        onClick={() => resetErrorBoundary()}
+      >
+        재시도하기
+      </Button>
+    </div>
+  );
+}
